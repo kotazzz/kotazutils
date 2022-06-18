bef5a9d757bbee3e203023686a230ea687625aca
Kotaz <semechkagent@gmail.com>
2022-06-19 00:44:16 +0300

    Убран мусорный файл, black

--- kotazutils/cli_tool.py
+++ kotazutils/cli_tool.py
@@ -4,8 +4,15 @@ from prompt_toolkit import ANSI, PromptSession
 
 from prompt_toolkit.completion import Completer, Completion
 
+
 class Command:
-    def __init__(self, name, brief='This is brief', description='This is description', arg_help=None):
+    def __init__(
+        self,
+        name,
+        brief="This is brief",
+        description="This is description",
+        arg_help=None,
+    ):
         self.name = name
         self.brief = brief
         self.description = description
@@ -25,18 +32,20 @@ class Command:
                     try:
                         args[i] = required_type(args[i])
                     except:
-                        errors.append([i, type(args[i]), required_type])      
+                        errors.append([i, type(args[i]), required_type])
 
     def callback(self, *args):
         return self.function(self, *args)
 
-    def subcommand(self, name, brief='This is brief', description='This is description'):
+    def subcommand(
+        self, name, brief="This is brief", description="This is description"
+    ):
         command = Command(name, brief, description)
         self.subcommands.append(command)
         return command
 
-class CliApp:
 
+class CliApp:
     def __init__(self, name, description, version):
         self.name = name
         self.description = description
@@ -53,51 +62,59 @@ class CliApp:
     def run(self):
         pass
 
+
 from typing import get_type_hints
 from rich.text import Text
 import io
+
 #! move to top ################################################################
 
 
 def mark_to_ansi(text, console=None):
     if console is None:
         console = Console(file=io.StringIO(), force_terminal=True)
-    console.print(text, markup=True, end='')
+    console.print(text, markup=True, end="")
     val = console.file.getvalue()
     console.file.seek(0)
     return ANSI(val)
 
 
-
 class HashableCompletion(Completion):
     def __hash__(self):
         return hash((self.text, id(self)))
-    
+
+
 # @Command("echo", "Echo", "Echo something")
 # def echo(self, text: str, times: int = 2, without_hello: bool = False):
 #     text = "Hello, " + text if not without_hello else text
 #     return ', '.join([text] * times)
-# 
+#
 # @echo.subcommand("test", "Test", "Test something")
 # def test(self, text: str = "test"):
 #     print("Test: " + text)
-# 
+#
 # print(echo.callback("Hello", 3, True))
 # print(test)
 # print(echo.print_help())
+
 
 class MyCustomCompleter(Completer):
     def update_completions(self, data):
         self.completions = data
 
-    
     def get_completions(self, document, complete_event):
         words = shlex.split(document.text_before_cursor)
         last_word = words[-1] if words else ""
         completions = self.completions
         text = document.text_before_cursor
+
         def data_to_completion(data):
-            return Completion(data.text.lstrip(last_word), display=data.display, display_meta=data.meta)
+            return Completion(
+                data.text.lstrip(last_word),
+                display=data.display,
+                display_meta=data.meta,
+            )
+
         def get_completions_for_word(word, completions, remain_words):
             # print(word, completions, remain_words)
             possible = {}
@@ -109,10 +126,17 @@ class MyCustomCompleter(Completer):
                         possible[completion] = nested
                     if word == completion.text:
                         if remain_words:
-                            return get_completions_for_word(remain_words.pop(0), nested, remain_words)
-                        elif text.endswith(' ') and nested:
-                            return nested.keys() if isinstance(nested, dict) else list(nested)
+                            return get_completions_for_word(
+                                remain_words.pop(0), nested, remain_words
+                            )
+                        elif text.endswith(" ") and nested:
+                            return (
+                                nested.keys()
+                                if isinstance(nested, dict)
+                                else list(nested)
+                            )
             return possible
+
         if words:
             c = get_completions_for_word(words.pop(0), completions, words)
             for data in c:
@@ -122,22 +146,31 @@ class MyCustomCompleter(Completer):
             for completion in completions:
                 yield data_to_completion(completion)
 
+
 class RichCompletion:
     def __init__(self, text, display, meta):
         self.text = text
         self.display = mark_to_ansi(display)
         self.meta = mark_to_ansi(meta)
-    
-    
+
+
 completions = {
-    RichCompletion("echo", "[bold]echo[/] <'test' \[text]|text>", "Выводит текст", ): {
-        RichCompletion("test", "[bold]test[/] \[text]", "Выводит тестовый текст", )
-        },
-    RichCompletion("echo2", "[bold]echo2[/] <Обязательный|'Подкоманда' \[Необязательный арг подкоманды]>", "Тестовая команда", ): None
-    }
+    RichCompletion("echo", "[bold]echo[/] <'test' \[text]|text>", "Выводит текст",): {
+        RichCompletion(
+            "test",
+            "[bold]test[/] \[text]",
+            "Выводит тестовый текст",
+        )
+    },
+    RichCompletion(
+        "echo2",
+        "[bold]echo2[/] <Обязательный|'Подкоманда' \[Необязательный арг подкоманды]>",
+        "Тестовая команда",
+    ): None,
+}
 
 c = MyCustomCompleter()
 c.update_completions(completions)
 
-session = PromptSession('> ', completer=c)
+session = PromptSession("> ", completer=c)
 text = session.prompt()
