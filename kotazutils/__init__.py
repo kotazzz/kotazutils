1592a1a132a07466d31a1c1e51564a5fecbfb7a2
Kotaz <semechkagent@gmail.com>
2022-06-19 02:42:23 +0300

    Создан пример, добавлен чейнджлог, билд 0.4

--- kotazutils/__init__.py
+++ kotazutils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = "Kotaz"
 __license__ = "MIT"
 __email__ = "semechkagent@gmail.com"
@@ -6,3 +6,11 @@ __email__ = "semechkagent@gmail.com"
 __description__ = """
 KotazUtils is a collection of utilities for Python.
 """
+
+# DONT FORGET TO ADD CHANGELOG
+__changelog__ = """
+0.4.0:
+    - Added `cli_tool` module.
+"""
+
+# python setup.py bdist_wheel
--- kotazutils/cli_tool.py
+++ kotazutils/cli_tool.py
@@ -274,26 +274,3 @@ class CliApp:
                 self.console.print_exception()
 
 
-c = CliApp("App")
-
-
-@c.command("echo", "Echo text", "Echo something")
-def echo(self, cli, text: str, times: int = 2, without_hello: bool = False):
-    text = "Hello, " + text if not without_hello else text
-    cli.console.print(", ".join([text] * times))
-
-
-@echo.subcommand("subcommand", "Echo text", "Echo something")
-def echo_subcommand(
-    self, cli, first_word: str, text: str, times: int = 2, without_hello: bool = False
-):
-    text = first_word + ", " + text if not without_hello else text
-    cli.console.print(", ".join([text] * times))
-
-
-@c.command("exit", "Exit from app", "Close this app")
-def exit_(self, cli):
-    cli.state = "exit"
-
-
-c.run()
